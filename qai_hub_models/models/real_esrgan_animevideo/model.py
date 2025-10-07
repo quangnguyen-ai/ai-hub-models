c5b38f2e9e863c25348e3aac7d6f97ee4103c69e
quang.nguyen <quang.nguyen@gremsy.com>
2025-10-07 09:24:59 +0700

    add nano models

--- qai_hub_models/models/real_esrgan_animevideo/model.py
+++ qai_hub_models/models/real_esrgan_animevideo/model.py
@@ -14,7 +14,7 @@ from qai_hub_models.models._shared.super_resolution.model import SuperResolution
 from qai_hub_models.utils.asset_loaders import SourceAsRoot
 
 REALESRGAN_SOURCE_REPOSITORY = "https://github.com/quangnguyen-ai/Real-ESRGAN"
-REALESRGAN_SOURCE_REPO_COMMIT = "1127f4c788f38f868fedce08bd17e3cc63f7cf18"
+REALESRGAN_SOURCE_REPO_COMMIT = "365adf67a94fcf572a21bde1ed9b98e1e5bc79c7"
 REALESRGAN_SOURCE_VERSION = 1
 MODEL_ID = __name__.split(".")[-2]
 MODEL_ASSET_VERSION = 2
@@ -57,8 +57,8 @@ def _get_weightsfile_from_name(weights_name: str = DEFAULT_WEIGHTS, scale: int =
     weights_map = {
         "realesr-animevideov3": "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-animevideov3.pth",
         "realesr-animevideox2v3": "https://github.com/quangnguyen-ai/Real-ESRGAN/raw/master/weights/realesr-animevideox2v3.pth",
-        "nanorealesr-animevideox2v3" :"https://github.com/quangnguyen-ai/Real-ESRGAN/raw/refs/heads/master/weights/smalll-realesr-animevideox2v3.pth",
-        "smallrealesr-animevideox2v3" :"https://github.com/quangnguyen-ai/Real-ESRGAN/raw/refs/heads/master/weights/deep32-realesr-animevideox2v3.pth",
+        "smallrealesr-animevideox2v3" :"https://github.com/quangnguyen-ai/Real-ESRGAN/raw/refs/heads/master/weights/smalll-realesr-animevideox2v3.pth",
+        "nanorealesr-animevideox2v3" :"https://github.com/quangnguyen-ai/Real-ESRGAN/raw/refs/heads/master/weights/nano-realesr-animevideox2v3.pth",
         "mediumrealesr-animevideox2v3" :"https://github.com/quangnguyen-ai/Real-ESRGAN/raw/refs/heads/master/weights/medium-realesr-animevideox2v3.pth",
     }
 
